5da188d6df8696203604d13ef7fb7bb31909620b
Arya Deep Chowdhury <arya.d.chowdhury@gmail.com>
2026-09-07 18:59:13 +0530

    Setup, Exception, Logging and Database Code Added

---
+++ src/database/mongodb.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+from src.logger import logging
+from src.exception import CustomException
+
+import sys
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+uri = os.getenv("MONGO_URI")
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    logging("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    raise CustomException(e, sys)
