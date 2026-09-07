0ab9dc961548dcb61e34cd8743ac103f57a9df2c
Arya Deep Chowdhury <arya.d.chowdhury@gmail.com>
2026-09-07 23:04:38 +0530

    Working Code

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def main():
-    print("Hello from churn-model!")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from src.pipeline.predict_pipeline import PredictPipeline
 
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="Customer Churn Prediction API")
+
+
+class CustomerData(BaseModel):
+
+    AccountWeeks: int
+    ContractRenewal: int
+    DataPlan: int
+    DataUsage: float
+    CustServCalls: int
+    DayMins: float
+    DayCalls: int
+    MonthlyCharge: float
+    OverageFee: float
+    RoamMins: float
+
+
+pipeline = PredictPipeline()
+
+
+@app.get("/")
+def home():
+    return {"message": "Customer Churn Prediction API is running"}
+
+
+@app.post("/predict")
+def predict(data: CustomerData):
+
+    features = data.model_dump()
+
+    prediction, probability = pipeline.predict(features)
+
+    result = "Churn" if prediction == 1 else "No Churn"
+
+    return {
+        "prediction": result,
+        "churn_probability": round(float(probability), 4)
+    }
